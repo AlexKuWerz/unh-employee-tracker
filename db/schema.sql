7ac01c2d1e40950afc99debb035d20e531e4152f
Oleksandr Kulyk <kylik.aleksandr@gmail.com>
2021-05-31 17:52:47 -0400

    Add database schema file.

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT REFERENCES employees(id),
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+-- CONSTRAINT sr_fk_employee_id FOREIGN KEY (manager_id) REFERENCES employees(id)
