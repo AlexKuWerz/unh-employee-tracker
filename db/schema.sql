0a09579fd804b204a64cb34e8eb2bdaddf71aa7f
Oleksandr Kulyk <kylik.aleksandr@gmail.com>
2021-06-01 00:55:42 -0400

    Update database schema file. Change db name.

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db;
+DROP DATABASE IF EXISTS employees_example_db;
+CREATE DATABASE employees_example_db;
 
-USE employees_db;
+USE employees_example_db;
 
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT,
